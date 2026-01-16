fe3ab6731d03fd010661aee435eabc45cf20d6c5
7minutech <elijohnson1107@gmail.com>
2026-01-15 18:36:57 -0600

    refac: updated the war handler function in the client to publish game logs

--- cmd/client/handler_war.go
+++ cmd/client/handler_war.go
@@ -6,9 +6,11 @@ import (
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func handlerWar(gs *gamelogic.GameState) func(gamelogic.RecognitionOfWar) pubsub.AckType {
+func handlerWar(gs *gamelogic.GameState, publishCh *amqp.Channel) func(gamelogic.RecognitionOfWar) pubsub.AckType {
 	return func(recogWar gamelogic.RecognitionOfWar) pubsub.AckType {
 		defer fmt.Print("> ")
 		warOutcome, winner, loser := gs.HandleWar(recogWar)
@@ -18,17 +20,40 @@ func handlerWar(gs *gamelogic.GameState) func(gamelogic.RecognitionOfWar) pubsub
 		case gamelogic.WarOutcomeNoUnits:
 			return pubsub.NackDiscard
 		case gamelogic.WarOutcomeOpponentWon:
-			log.Printf("%s won a war against %s", winner, loser)
+			msg := fmt.Sprintf("%s won a war against %s", winner, loser)
+			gl := routing.GameLog{Username: gs.GetUsername(), Message: msg}
+			err := publishGameLog(gl, publishCh)
+			if err != nil {
+				return pubsub.NackRequeue
+			}
 			return pubsub.Ack
 		case gamelogic.WarOutcomeYouWon:
-			log.Printf("%s won a war against %s", winner, loser)
+			msg := fmt.Sprintf("%s won a war against %s", winner, loser)
+			gl := routing.GameLog{Username: gs.GetUsername(), Message: msg}
+			err := publishGameLog(gl, publishCh)
+			if err != nil {
+				return pubsub.NackRequeue
+			}
 			return pubsub.Ack
 		case gamelogic.WarOutcomeDraw:
-			log.Printf("A war between %s and %s resulted in a draw", winner, loser)
+			msg := fmt.Sprintf("A war between %s and %s resulted in a draw", winner, loser)
+			gl := routing.GameLog{Username: gs.GetUsername(), Message: msg}
+			err := publishGameLog(gl, publishCh)
+			if err != nil {
+				return pubsub.NackRequeue
+			}
 			return pubsub.Ack
 		default:
 			log.Print("error: handling war outcome")
 			return pubsub.NackDiscard
 		}
 	}
+}
+
+func publishGameLog(gl routing.GameLog, publishCh *amqp.Channel) error {
+	err := pubsub.PublishGob(publishCh, routing.ExchangePerilTopic, routing.GameLogSlug+gl.Username, gl)
+	if err != nil {
+		return err
+	}
+	return nil
 }
