ea328a62a1db0610de1b8947f3836dd32cd4c653
7minutech <elijohnson1107@gmail.com>
2026-01-15 18:25:05 -0600

    refac: handlerWar outputs logs depending on war won or tie

--- cmd/client/handler_war.go
+++ cmd/client/handler_war.go
@@ -11,17 +11,20 @@ import (
 func handlerWar(gs *gamelogic.GameState) func(gamelogic.RecognitionOfWar) pubsub.AckType {
 	return func(recogWar gamelogic.RecognitionOfWar) pubsub.AckType {
 		defer fmt.Print("> ")
-		warOutcome, _, _ := gs.HandleWar(recogWar)
+		warOutcome, winner, loser := gs.HandleWar(recogWar)
 		switch warOutcome {
 		case gamelogic.WarOutcomeNotInvolved:
 			return pubsub.NackRequeue
 		case gamelogic.WarOutcomeNoUnits:
 			return pubsub.NackDiscard
 		case gamelogic.WarOutcomeOpponentWon:
+			log.Printf("%s won a war against %s", winner, loser)
 			return pubsub.Ack
 		case gamelogic.WarOutcomeYouWon:
+			log.Printf("%s won a war against %s", winner, loser)
 			return pubsub.Ack
 		case gamelogic.WarOutcomeDraw:
+			log.Printf("A war between %s and %s resulted in a draw", winner, loser)
 			return pubsub.Ack
 		default:
 			log.Print("error: handling war outcome")
