8901219e9a03729ea672e9c043beb584fa8c3d2e
7minutech <elijohnson1107@gmail.com>
2026-01-15 18:21:57 -0600

    feat: PublishGob

---
+++ internal/pubsub/publish_gob.go
@@ -0,0 +1,23 @@
+package pubsub
+
+import (
+	"bytes"
+	"context"
+	"encoding/gob"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishGob[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(val)
+	if err != nil {
+		return err
+	}
+	data := buf.Bytes()
+
+	ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{ContentType: "application/gob", Body: data})
+
+	return nil
+}
